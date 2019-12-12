02dc781ae2752270f72f890cf76d63757ad6715b
josh Hill <Josh_hill15@me.com>
2019-12-12 18:55:19 -0500

    mvpp

--- react-redux-app/src/App.js
+++ react-redux-app/src/App.js
@@ -3,18 +3,45 @@ import './App.css';
 import { connect } from "react-redux"
 import Loader from "react-loader-spinner"
 import { getFoxes } from "./actions/action"
+import styled from "styled-components"
 
+const Button = styled.button`
+background: aqua;
+padding:.5% 1.5%;
+box-shadow: 5px 10px 13px #888888;
+margin-bottom: 2%;
+
+  &:hover{
+    color:aqua;
+    background:black
+  }
+`
+const Header = styled.h1`
+  background: lightblue;
+  color:white;
+  box-shadow: 5px 10px 13px #888888;
+
+`
+const Img = styled.img`
+  max-height: 400px;
+  max-width: 400px;
+  margin-top:3%;
+  border:none;
+  box-shadow: 5px 10px 13px #888888;
+
+`
 function App(props) {
   console.log("app props", props)
   return (
     <div className="App">
-      <h1>Fox Pictures!</h1>
+      <Header>Fox Pictures!</Header>
        {!props.foxes && !props.isFetching && <p> Get some pics of Foxes! </p> }
+      {!props.isFetching && <Button onClick = {props.getFoxes}>More Foxes!</Button>}
+      <br/>
       {props.isFetching && (
         <Loader type="Puff" color="#00BFFF" height={100} width={100} />
       )}
-      <button onClick = {props.getFoxes}>More Foxes!</button><br/>
-      {props.foxes && <img className = "pic" src = {props.foxes.image} />}
+      {props.foxes && <Img src = {props.foxes.image} />}
     </div>
   );
 }
